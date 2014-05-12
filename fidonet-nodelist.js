4ddab994df7c15596875dcb5d81990923270f5fb
Mithgol the Webmaster <getgit@mithgol.ru>
2014-05-12 19:25:04 +0400

    split a string to lines, split a line to fields

--- fidonet-nodelist.js
+++ fidonet-nodelist.js
@@ -1,5 +1,24 @@
+var fs = require('fs');
+
 var nodelist = function(nodelistPath){
    if (!(this instanceof nodelist)) return new nodelist(nodelistPath);
+
+   var nodelistString = fs.readFileSync(nodelistPath, {
+      encoding: 'utf8'
+   });
+   if( nodelistString.slice(-1) === '\x1A' ){ // EOF
+      nodelistString = nodelistString.slice(-1);
+   }
+   var nodelistLines = nodelistString.split( /\x0d?\x0a/ );
+   nodelistString = null;
+
+   var currentZone    = null;
+   var currentRegion  = null;
+   var currentNet     = null;
+   var currentHub     = null;
+   nodelistLines.forEach(function(line){
+      var fields = line.split(',');
+   });
 };
 
 module.exports = nodelist;
