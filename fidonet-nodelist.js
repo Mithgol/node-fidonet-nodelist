d67384b18e70c19dc53f6f5267885871b4febff7
Mithgol the Webmaster <getgit@mithgol.ru>
2014-05-16 10:46:40 +0400

    require array.prototype.findindex; find zones, and regions, and nets

--- fidonet-nodelist.js
+++ fidonet-nodelist.js
@@ -1,3 +1,4 @@
+require('array.prototype.findindex');
 var fs = require('fs');
 
 var nodelist = function(nodelistPath){
@@ -15,6 +16,62 @@ var nodelist = function(nodelistPath){
    this.nodelistLines = nodelistLines.filter(function(line){
       return line.length > 0 && line.indexOf(';') !== 0;
    });
+};
+
+nodelist.prototype.getLineForNode = function(node){
+   node = '' + node;
+   if(! /^\d+:\d+\/\d+$/.test(node) ) return null;
+   var matches, reZone;
+
+   matches = /^(\d+):\1\/0$/.exec(node);
+   if( matches !== null ){ // zone
+      reZone = RegExp('^Zone,' + matches[1] + ',');
+      var idx = this.nodelistLines.findIndex(function(line){
+         // `this` contains regex
+         return this.test(line);
+      }, reZone);
+      if( idx < 0 ){
+         return null;
+      } else return this.nodelistLines[idx];
+   }
+
+   matches = /^(\d+):(\d+)\/0$/.exec(node);
+   if( matches !== null ){ // region or net
+      reZone = RegExp('^Zone,' + matches[1] + ',');
+      var idxZone = this.nodelistLines.findIndex(function(line){
+         // `this` contains regex
+         return this.test(line);
+      }, reZone);
+      if( idxZone < 0 ) return null;
+
+      var idxZoneNext = this.nodelistLines.findIndex(function(line, idx){
+         // `this.prevIDX` contains index of the previous zone
+         if( idx <= this.prevIDX ) return false;
+         // `this.reZone` contains regex
+         return this.reZone.test(line);
+      }, {
+         prevIDX: idxZone,
+         reZone: /^Zone,/
+      });
+      if( idxZoneNext < 0 ) idxZoneNext = this.nodelistLines.length;
+
+      var reRegNet = RegExp('^(?:Region|Host),' + matches[2] + ',');
+      var idxRegNet = this.nodelistLines.findIndex(function(line, idx){
+         // `this.prevIDX` contains index of the previous zone
+         if( idx <= this.prevIDX ) return false;
+         // `this.nextIDX` contains index of the next zone
+         if( idx >= this.nextIDX ) return false;
+         // `this.reRegNet` contains regex
+         return this.reRegNet.test(line);
+      }, {
+         prevIDX: idxZone,
+         nextIDX: idxZoneNext,
+         reRegNet: reRegNet
+      });
+      if( idxRegNet < 0 ){
+         return null;
+      } else return this.nodelistLines[idxRegNet];
+   }
 };
 
 module.exports = nodelist;
--- test/test.js
+++ test/test.js
@@ -9,17 +9,66 @@ var testLine2 = ',88,FGHI_Global_Headlight_Ignited,Gelendzhik,' +
                 'Sergey_Sokoloff,00-00-000000,300,IBN:1978,' +
                 'INA:Fidonet.Mithgol.Ru,U,TSU';
 
+var lineZone2 = 'Zone,2,Eur_(122),B,Ward_Dossche,32-3-4480880,33600,V34,' +
+                'VFC,ICM,XX,IEM,ITX,IBN,TXH';
+var lineZone2Reg50 = 'Region,50,Russia,Moscow,Alex_Barinov,-Unpublished-,' +
+                     '300,CM,XX,V90S,IBN,INA:fido.hubahuba.su,' +
+                     'IRD:binkp.net,U,DO4:binkp.net';
+var lineZone2Net5063 = 'Host,5063,Kuban_Net,Krasnodar_Russia,' +
+                       'Igor_Grabelnikov,7-861-222-8470,9600,XX,MO,CM,' +
+                       'V42B,HST,V32T,V34';
+
 describe('Fidonet nodelist reader', function(){
-   var nodelist = require('../')( path.join(__dirname, 'nodelist.txt') );
+   var nodelist;
    var idx;
 
-   it('the "FGHI Global Headlight Ignited" node can be found', function(){
+   it('the nodelist is read, lines are parsed', function(){
+      nodelist = require('../')( path.join(__dirname, 'nodelist.txt') );
+      assert(nodelist.nodelistLines.length > 0);
+   });
+
+   it('the "FGHI Global Headlight Ignited" node can be read', function(){
       idx = nodelist.nodelistLines.indexOf(testLine);
       assert.notEqual(idx, -1);
    });
 
-   it("almost the same node's line can be found in another net", function(){
+   it("almost the same node's line is read from another net", function(){
       var idx2 = nodelist.nodelistLines.indexOf(testLine2, idx+1);
       assert.notEqual(idx2, -1);
+   });
+
+   it("can find the line of Zone 2 by number", function(){
+      var line = nodelist.getLineForNode('2:2/0');
+      assert.equal(line, lineZone2);
+   });
+
+   it("can't find the line of Zone 7 by number", function(){
+      var line = nodelist.getLineForNode('7:7/0');
+      assert.strictEqual(line, null);
+   });
+
+   it("can find the line of Region 2:50 by number", function(){
+      var line = nodelist.getLineForNode('2:50/0');
+      assert.equal(line, lineZone2Reg50);
+   });
+
+   it("can't find the line of Region 2:999 by number", function(){
+      var line = nodelist.getLineForNode('2:999/0');
+      assert.strictEqual(line, null);
+   });
+
+   it("can't find the line of Region 1:50 by number", function(){
+      var line = nodelist.getLineForNode('1:50/0');
+      assert.strictEqual(line, null);
+   });
+
+   it("can't find the line of Region 3:50 by number", function(){
+      var line = nodelist.getLineForNode('3:50/0');
+      assert.strictEqual(line, null);
+   });
+
+   it("can find the line of Net 2:5063 by number", function(){
+      var line = nodelist.getLineForNode('2:5063/0');
+      assert.equal(line, lineZone2Net5063);
    });
 });
