a4272d7ceab77fc1c251a0bb843bbdefa4a72c67
Mithgol the Webmaster <getgit@mithgol.ru>
2014-05-17 00:14:16 +0400

    simplify things: single `.exec()` for matches, linear script below

--- fidonet-nodelist.js
+++ fidonet-nodelist.js
@@ -20,58 +20,53 @@ var nodelist = function(nodelistPath){
 
 nodelist.prototype.getLineForNode = function(node){
    node = '' + node;
-   if(! /^\d+:\d+\/\d+$/.test(node) ) return null;
-   var matches, reZone;
 
-   matches = /^(\d+):\1\/0$/.exec(node);
-   if( matches !== null ){ // zone
-      reZone = RegExp('^Zone,' + matches[1] + ',');
-      var idx = this.nodelistLines.findIndex(function(line){
-         // `this` contains regex
-         return this.test(line);
-      }, reZone);
-      if( idx < 0 ){
-         return null;
-      } else return this.nodelistLines[idx];
+   var matches = /^(\d+):(\d+)\/(\d+)$/.exec(node);
+   if( matches === null ) return null;
+
+   var reZone = RegExp('^Zone,' + matches[1] + ',');
+   var idxZone = this.nodelistLines.findIndex(function(line){
+      // `this` contains regex
+      return this.test(line);
+   }, reZone);
+   if( idxZone < 0 ) return null;
+
+   if( matches[3] === '0' && matches[1] === matches[2] ){
+      // zone mode
+      return this.nodelistLines[idxZone];
    }
 
-   matches = /^(\d+):(\d+)\/0$/.exec(node);
-   if( matches !== null ){ // region or net
-      reZone = RegExp('^Zone,' + matches[1] + ',');
-      var idxZone = this.nodelistLines.findIndex(function(line){
-         // `this` contains regex
-         return this.test(line);
-      }, reZone);
-      if( idxZone < 0 ) return null;
+   var idxZoneNext = this.nodelistLines.findIndex(function(line, idx){
+      // `this.prevIDX` contains index of the previous zone
+      if( idx <= this.prevIDX ) return false;
+      // `this.reZone` contains regex
+      return this.reZone.test(line);
+   }, {
+      prevIDX: idxZone,
+      reZone: /^Zone,/
+   });
+   if( idxZoneNext < 0 ) idxZoneNext = this.nodelistLines.length;
 
-      var idxZoneNext = this.nodelistLines.findIndex(function(line, idx){
-         // `this.prevIDX` contains index of the previous zone
-         if( idx <= this.prevIDX ) return false;
-         // `this.reZone` contains regex
-         return this.reZone.test(line);
-      }, {
-         prevIDX: idxZone,
-         reZone: /^Zone,/
-      });
-      if( idxZoneNext < 0 ) idxZoneNext = this.nodelistLines.length;
+   var reRegNet = RegExp('^(?:Region|Host),' + matches[2] + ',');
+   var idxRegNet = this.nodelistLines.findIndex(function(line, idx){
+      // `this.prevIDX` contains index of the previous zone
+      if( idx <= this.prevIDX ) return false;
+      // `this.nextIDX` contains index of the next zone
+      if( idx >= this.nextIDX ) return false;
+      // `this.reRegNet` contains regex
+      return this.reRegNet.test(line);
+   }, {
+      prevIDX: idxZone,
+      nextIDX: idxZoneNext,
+      reRegNet: reRegNet
+   });
+   if( idxRegNet < 0 ) return null;
 
-      var reRegNet = RegExp('^(?:Region|Host),' + matches[2] + ',');
-      var idxRegNet = this.nodelistLines.findIndex(function(line, idx){
-         // `this.prevIDX` contains index of the previous zone
-         if( idx <= this.prevIDX ) return false;
-         // `this.nextIDX` contains index of the next zone
-         if( idx >= this.nextIDX ) return false;
-         // `this.reRegNet` contains regex
-         return this.reRegNet.test(line);
-      }, {
-         prevIDX: idxZone,
-         nextIDX: idxZoneNext,
-         reRegNet: reRegNet
-      });
-      if( idxRegNet < 0 ){
-         return null;
-      } else return this.nodelistLines[idxRegNet];
+   if( matches[3] === '0' ){
+      // region or net mode
+      return this.nodelistLines[idxRegNet];
    }
+
 };
 
 module.exports = nodelist;
